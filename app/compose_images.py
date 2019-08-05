0b212260e04cb7d68fbca2c092ff614b1378b1f3
Skyler Burger <skylerburger@gmail.com>
2019-08-05 15:04:35 -0700

    image composition functions

---
+++ app/compose_images.py
@@ -0,0 +1,42 @@
+from PIL import Image
+
+#####################
+#    Test Images    #
+#####################
+imgsFolder = [
+ 'assets/style_1/t_1.png',
+ 'assets/style_1/h_1.png',
+ 'assets/style_1/i_1.png',
+ 'assets/style_1/n_1.png',
+ 'assets/style_1/k_1.png'
+]
+
+
+def determine_canvas(input_images):
+    images = [Image.open(image) for image in input_images]
+    
+    sum_width = 0
+    max_height = 0
+    for img in images:
+        width, height = img.size
+        sum_width += width
+        if height > max_height:
+            max_height = height
+
+    sum_width +=20
+    max_height += 20
+
+    return compose_image(sum_width, max_height, images)
+
+
+def compose_image(width, height, images):
+    background = Image.new('RGAB', (width, height), (255, 255, 255, 255))
+    width_offset = 10
+
+    for img in images:
+        img_width, img_height = img.size
+        offset = (width_offset, (height - img_height) // 2)
+        width_offset += img_width
+        background.paste(img, offset)
+
+    background.save('output.png')
