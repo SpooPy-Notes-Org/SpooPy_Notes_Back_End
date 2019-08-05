ba7772906b0ae8db582154d3548c4103f0a427cf
arsummers <aliyasummers1@gmail.com>
2019-08-05 16:21:45 -0700

    single image successfully opens

--- app/letters/letters.py
+++ app/letters/letters.py
@@ -1,27 +1,29 @@
 import os
 from os.path import abspath, join
 import random
+from PIL import Image
 
 
 # contains pathing to the letters
-# create dictionary
 # f string... path f'{letter}_{random_num}.png'
+def read_image(path):
 
-class Letters():
+    try:
+        img = Image.open(path)
+        img.show()
+    except FileNotFoundError:
+        print('no file found fix pathing: you got this', path)
+
+class Letters:
+
     rand = random.randint(1, 3)
-    base_letter_path = 'assets/'
+    base_letter_path = 'app/assets/'
 
     # letter should be set to ones from query string
-    # a = f'{base_letter_path}a_{rand}'
     a = f'{base_letter_path}a_{rand}.png'
-    # b = f'{base_letter_path}b_{rand}.png'
-    # c = f'{base_letter_path}c_{rand}.png'
 
     result = join(os.getcwd(), abspath(a))
 
-    try:
-        with open(result, 'r') as file:
-            contents = file.read()
-            print('We have reached the thing', contents)
-    except FileNotFoundError:
-        print('no file found fix pathing')
+    read_image(result)
+
+
