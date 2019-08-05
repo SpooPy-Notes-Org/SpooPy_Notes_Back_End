4ca7ce29df02fed28aa98b5071b66294a77a35c3
Joshua Ho <ho.joshua4@gmail.com>
2019-08-05 14:46:24 -0700

    Beginning composeImages feature

---
+++ app/compose_image.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+
+#####################
+#    Test Images    #
+#####################
+
+imgsFolder = [
+  'assets/style_1/t_1.png',
+  'assets/style_1/h_1.png',
+  'assets/style_1/i_1.png',
+  'assets/style_1/n_1.png',
+  'assets/style_1/k_1.png'
+]
