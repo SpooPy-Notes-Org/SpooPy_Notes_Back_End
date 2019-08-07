4e6f16eecd9c944c9bdd93a62818f799da9e7ffb
tammytdo11 <tammytdo11@gmail.com>
2019-08-07 16:34:23 -0700

    added dad jokes api

--- app/spoopy.py
+++ app/spoopy.py
@@ -2,6 +2,7 @@ from PIL import Image
 from flask import send_file
 import os
 import random
+import re
 from os.path import abspath, join
 from io import BytesIO
 
@@ -156,53 +157,54 @@ def serve_pil_image(pil_img):
     return send_file(img_io, mimetype='image/png')
 
 
-"""
-input = a string, the message you want to enter
-
-special_ chars = a dictionary of punctuation marks that is accessed with the query
-
-generate paths = a function that builds dynamic pathing to each character in the query, so that the images can be used in a personalized ransom note
-"""
-
 base_punctuation_path = 'app/assets/punctuation_1'
 
 special_chars = {
-    '&': f'{base_punctuation_path}/ampersand_1.png',
     '\'': f'{base_punctuation_path}/apostrophe_1.png',
-    '*': f'{base_punctuation_path}/asterisk_1.png',
-    '@': f'{base_punctuation_path}/at_1.png',
     '\"': f'{base_punctuation_path}/doublequote_straight_1.png',
-    '!': f'{base_punctuation_path}/exclaimation_1.png',
+    '\n': ' ',
+    '\t': ' ',
+    '\n': ' ',
+    ' ': ' ',
+    '@': f'{base_punctuation_path}/at_1.png',
     '$': f'{base_punctuation_path}/money_1.png',
-    '%': f'{base_punctuation_path}/percentage_1.png',
-    '.': f'{base_punctuation_path}/period_1.png',
     '#': f'{base_punctuation_path}/pound_1.png',
+    ':': f'{base_punctuation_path}/colon_1.png',
+    ';': f'{base_punctuation_path}/semicolon_1.png',
+    '.': f'{base_punctuation_path}/period_1.png',
+    '…': f'{base_punctuation_path}/ellipses_1.png',
+    '-': f'{base_punctuation_path}/hyphen_1.png',
+    '*': f'{base_punctuation_path}/asterisk_1.png',
+    '&': f'{base_punctuation_path}/ampersand_1.png',
+    '%': f'{base_punctuation_path}/percentage_1.png',
+    '!': f'{base_punctuation_path}/exclaimation_1.png',
     '?': f'{base_punctuation_path}/questionmark_1.png',
     ',': f'{base_punctuation_path}/comma_1.png',
-    ' ': ' ' 
-}
+    '‘': f'{base_punctuation_path}/apostrophe_1.png',
+    '’': f'{base_punctuation_path}/apostrophe_1.png',
+    '”': f'{base_punctuation_path}/doublequote_straight_1.png',
+    '“': f'{base_punctuation_path}/doublequote_straight_1.png',
+    }
 
 
 def generate_paths(query_string):
     query_string = query_string.lower()
     path_list = []
+    regex_object = re.compile(r'[a-z0-9]')
+    
     for char in query_string:
-
         rand = random.randint(1, 4)
         base_letter_path = 'app/assets/'
         
-        letter = f'{base_letter_path}{char}_{rand}.png'
-
-        if char in special_chars:
-            path_list.append(special_chars[char])
-            continue
-
-        # error handling - checks if character is alphanumeric or in special characters
-        if char.isalnum() == False or char in special_chars:
-            raise ValueError(f'Invalid character input: {char}')
-
-        path_list.append(letter)
+        match_object = regex_object.search(char)
         
+        if match_object:
+            path_list.append(f'{base_letter_path}{char}_{rand}.png')
+        elif char in special_chars:
+            path_list.append(special_chars[char])
+        else:
+            path_list.append(' ')
+
     return path_list
 
 
